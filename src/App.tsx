2f4402309dd9ccaf1a572832f4de31bdf62ca05c
Sebasm2kuy <169087693+Sebasm2kuy@users.noreply.github.com>
2025-07-17 21:34:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
 import React from 'react';
-import Header from './components/Header';
+import './App.css';
+
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ProductosDestacados from './components/ProductosDestacados';
-import AboutSection from './components/About';
-import ContactSection from './components/Contact';
+import About from './components/About';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="pt-20"> {/* Espacio para el header fijo */}
-      <Header />
-
-      <section id="hero">
-        <Hero />
-      </section>
-
-      <section id="productos">
-        <ProductosDestacados />
-      </section>
-
-      <section id="about">
-        <AboutSection />
-      </section>
-
-      <section id="contact">
-        <ContactSection />
-      </section>
-
+    <>
+      <Navbar />
+      <Hero />
+      <ProductosDestacados />
+      <About />
+      <Contact />
       <Footer />
-    </div>
+    </>
   );
-};
+}
 
 export default App;
