974c1c201094685e68835670e4fc30b19ebaf34e
sebacabrera1982 <sebacabrera1982@gmail.com>
2025-07-19 01:11:31 +0000

    Limpiar configuración de Firebase

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,10 @@
+// src/config/firebase.ts
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +18,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Export Firebase services to be used in other parts of your app
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export default app;
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+// src/main.tsx
 
-// Configurar variables globales para Firebase
-globalThis.__app_id = 'aluna-admin-panel';
-globalThis.__firebase_config = {
-  apiKey: "demo-api-key",
-  authDomain: "demo-project.firebaseapp.com", 
-  projectId: "demo-project",
-  storageBucket: "demo-project.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "demo-app-id"
-};
-globalThis.__initial_auth_token = 'demo-token-123';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>,
+)
