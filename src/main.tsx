eaf7d50e543f8848227685e2701e400b2929d5b5
Sebasm2kuy <169087693+Sebasm2kuy@users.noreply.github.com>
2025-07-19 13:07:18 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ErrorBoundary } from './components/ErrorBoundary' // Ajusta la ruta si lo pusiste en otro lado
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
 )
